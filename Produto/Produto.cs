018abb1824e62154e0f357e55d44702053d9e549
OQueLucas <lucasestevam365@gmail.com>
2023-05-21 13:47:54 -0300

    Adicionando auto properties para reducao do codigo e facilidade de leitura

--- Produto/Produto.cs
+++ Produto/Produto.cs
@@ -6,8 +6,8 @@ namespace Produto
     internal class Produto
     {
         private string _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco { get; private set; }
+        private int Quantidade { get; private set; }
 
         public Produto()
         {
@@ -15,8 +15,8 @@ namespace Produto
         public Produto(string nome, double preco, int quantidade)
         {
             _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Preco = preco;
+            Quantidade = quantidade;
         }
 
         public string Nome
@@ -32,43 +32,27 @@ namespace Produto
             }
         }
 
-        public double Preco
-        {
-            get
-            {
-                return _preco;
-            }
-        }
-        public double Quantidade
-        {
-            get
-            {
-                return _quantidade;
-            }
-        }
-
-
         public double ValorTotalEmEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
 
         public void AdicionarProdutos(int quantidade)
         {
-            _quantidade += quantidade;
+            Quantidade += quantidade;
         }
         public void RemoverProdutos(int quantidade)
         {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
 
         public override string ToString()
         {
             return _nome 
                 + ", $" 
-                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + _quantidade
+                + Quantidade
                 + " unidades, Total: $"
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
